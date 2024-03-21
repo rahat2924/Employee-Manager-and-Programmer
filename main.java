0d0aeb4dc9fc652d85d1e4d434d8dbe374da0f22
RAHAT <105932806+rahat2924@users.noreply.github.com>
2024-03-21 06:20:23 +0600

    Create main.java

---
+++ main.java
@@ -0,0 +1,22 @@
+ abstract class Employee {
+ public abstract double
+ calculateSalary();
+ public abstract void displayInfo();
+ }
+ class Manager extends Employee {
+ // Implement calculateSalary() and
+ displayInfo() methods for manager
+ }
+ class Programmer extends Employee
+ {
+ // Implement calculateSalary() and
+ displayInfo() methods for programmer
+ }
+public class Main {
+ public static void main(String[]
+ args) {
+ // Code to test Manager and
+ Programmer classes
+ }
+ }
+ 
